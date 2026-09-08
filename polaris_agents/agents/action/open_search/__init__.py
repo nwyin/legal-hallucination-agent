2a1dd7034592c788ecda1174f0f9967291a1b0a2
phi <phi@phis-MacBook-Neo.local>
2026-09-08 11:52:59 -0700

    Remove unused search presentation utilities

--- polaris_agents/agents/action/open_search/__init__.py
+++ polaris_agents/agents/action/open_search/__init__.py
@@ -3,8 +3,5 @@
 from .search import search, search_results_xml, SearchResult
 from .serpapi import SerpApiClient
 from .mediastack import MediaStackClient
-from .actions import search_and_return_xml, convert_search_results_to_markdown, get_search_result_urls, render_markdown_results_for_jupyter
 
-__all__ = ["search", "search_results_xml", "SearchResult", "SerpApiClient", "MediaStackClient", 
-           "search_and_return_xml", "convert_search_results_to_markdown", "get_search_result_urls", 
-           "render_markdown_results_for_jupyter"]
+__all__ = ["search", "search_results_xml", "SearchResult", "SerpApiClient", "MediaStackClient"]
